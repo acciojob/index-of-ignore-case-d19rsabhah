765b2ffb7b45d355995ad0429c639d60e32f08c3
Rishav Das <rishavdas27602@gmail.com>
2024-12-12 22:42:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-function indexOfIgnoreCase(s1, s2) {
-  // write your code here
-}
+// function indexOfIgnoreCase(s1, s2) {
+//   // write your code here
+// }
+function indexOfIgnoreCase(str, subStr) {
+    // Convert both strings to lowercase for case-insensitive comparison
+    const lowerStr = str.toLowerCase();
+    const lowerSubStr = subStr.toLowerCase();
 
+    // Use the indexOf method to find the first occurrence of the substring
+    return lowerStr.indexOf(lowerSubStr);
+}
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
